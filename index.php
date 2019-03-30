2a5d0565242e3df414a12213569bc593ea03b3e7
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 19:16:24 -0400

    Color fix

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
      <div class="container">
        <nav class="navbar bg-secondary">
          <a class="navbar-brand" href="#"><h2>Noah's Cloud</h2></a>
-         <a class="navbar-item text-info" href="#"><h3>About</h3></a>
-         <a class="navbar-item text-info" href="#"><h3>Projects</h3></a>
-         <a class="navbar-item text-info" href="#"><h3>Login</h3></a>
+         <a class="navbar-item" href="#"><h3>About</h3></a>
+         <a class="navbar-item" href="#"><h3>Projects</h3></a>
+         <a class="navbar-item" href="#"><h3>Login</h3></a>
        </nav>
        </div>
      </div>
