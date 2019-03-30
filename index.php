5104043ead25da9928611cb85eb0e71fa41acdbc
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 19:15:50 -0400

    navbar coloring

--- index.php
+++ index.php
@@ -13,9 +13,11 @@
    </head>
    <body>
      <div class="container">
-       <nav class="navbar">
-         <a class="navbar-brand" href="#"><h1>Noah's Cloud</h1></a>
-         <a class="navbar-item" href="#"><h2>About</h2></a>
+       <nav class="navbar bg-secondary">
+         <a class="navbar-brand" href="#"><h2>Noah's Cloud</h2></a>
+         <a class="navbar-item text-info" href="#"><h3>About</h3></a>
+         <a class="navbar-item text-info" href="#"><h3>Projects</h3></a>
+         <a class="navbar-item text-info" href="#"><h3>Login</h3></a>
        </nav>
        </div>
      </div>
