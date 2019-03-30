38a41713ffdf3045bd326e2b08514185609bc539
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 19:57:05 -0400

    navbar tweek

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
      <div class="container">
        <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
-    <ul class="navbar-nav">
+    <ul class="">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home</a>
       </li>
