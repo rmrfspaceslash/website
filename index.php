e05f59ab613c531c7b6e4c14f44a78ba31e12c8e
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 18:55:24 -0400

    Bootstrap integration

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Noah's Cloud</title>
+     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+   </head>
+   <body>
+     <div class="container-fluid">
+       <nav class="navbar">
+
+       </nav>
+
+       </div>
+     </div>
+   </body>
+ </html>
