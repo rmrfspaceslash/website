3ec57dbb355f36b3c61e1ea158c845f7d2222686
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 20:35:11 -0400

    carousel fix

--- index.php
+++ index.php
@@ -42,10 +42,10 @@
       <div id="carousel" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
           <div class="carousel-item active">
-            <img class="d-block w-100" src="#" data-src="img/holder1.jpg" alt="First slide">
+            <img class="d-block w-100" src="img/holder2.jpg" data-src="img/holder1.jpg" alt="First slide">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="#" data-src="img/holder2.jpg" alt="Second slide">
+            <img class="d-block w-100" src="img/holder2.jpg" data-src="img/holder2.jpg" alt="Second slide">
           </div>
         </div>
         <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
