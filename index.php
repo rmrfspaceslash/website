c35dbae9246194d14c5f03b4636a7af51b7188bb
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 21:11:07 -0400

    test modal

--- index.php
+++ index.php
@@ -40,9 +40,11 @@
 
 
       <!--Login/Register Modal -->
+      <button class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">
+        Large modal
+      </button>
 
-
-      <div class="modal fade login-modal" tabindex="-1" role="dialog"
+      <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog"
            aria-labelledby="myLargeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
           <div class="modal-content">
