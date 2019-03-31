e2b1c2fa023cedbe7c11acabfa2a5e0d6f594ab0
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 20:37:53 -0400

    carousel fix

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
       </nav>
 
       <!--Beginning carousel code -->
-      <div id="carousel" class="carousel slide" data-ride="carousel">
+      <div id="carouselmain" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
           <div class="carousel-item active">
             <img class="d-block w-100" src="img/holder1.jpg" data-src="img/holder1.jpg" alt="First slide">
@@ -48,11 +48,11 @@
             <img class="d-block w-100" src="img/holder2.jpg" data-src="img/holder2.jpg" alt="Second slide">
           </div>
         </div>
-        <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+        <a class="carousel-control-prev" href="#carouselmain" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#carouselmain" role="button" data-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
