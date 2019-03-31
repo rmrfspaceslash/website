11e630a062d9e773c7134c0354b055b5c75265b8
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 20:26:55 -0400

    carousel

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
    </head>
     <body>
      <div class="container">
+
+       <!--navbar code -->
        <nav class="navbar navbar-expand-sm bg-light navbar-light justify-content-end">
          <a class="navbar-brand mr-auto ml-1" href="#">Noah's Cloud</a>
          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
@@ -35,6 +37,27 @@
            </ul>
         </div>
       </nav>
+
+      <!--Beginning carousel code -->
+      <div id="carousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="https://unsplash.com/photos/aIY_8IjPj3I" alt="First Slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="https://unsplash.com/photos/aIY_8IjPj3I" alt="Second Slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
     </div>
    </body>
  </html>
