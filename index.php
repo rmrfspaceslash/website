5a107d7f4c869b6ef2f60a4dc74eb6b8ace9447b
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 22:19:45 -0400

    remove href

--- index.php
+++ index.php
@@ -24,13 +24,13 @@
          <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
            <ul class="navbar-nav text-right">
              <li class="nav-item">
-               <a class="nav-link btn btn-link" href="#">About</a>
+               <a class="nav-link btn btn-link">About</a>
              </li>
              <li class="nav-item">
-               <a class="nav-link btn btn-link" href="#">Contact</a>
+               <a class="nav-link btn btn-link">Contact</a>
              </li>
              <li class="nav-item">
-               <a class="nav-link btn btn-link" href="#">Projects</a>
+               <a class="nav-link btn btn-link">Projects</a>
              </li>
              <li class="nav-item">
                <a class="nav-link btn btn-link" data-toggle="modal" data-target=".login-modal-lg">Login/Register</a>
