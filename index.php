15e65738c6188eda7f3f27e54e4c244250de167d
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 21:54:37 -0400

    color sight

--- index.php
+++ index.php
@@ -91,7 +91,7 @@
 
     <!--master header image -->
     <header class="masthead">
-      <div class="container h-100">
+      <div class="container h-100 bg-dark" >
         <div class="row h-100 align-items-center">
           <div class="col-12 text-center">
             <h1 class="font-weight-light">Vertically Centered Masthead Content</h1>
