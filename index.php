70c7b469ed58e06faf5cd844d914432eadb98922
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 21:12:13 -0400

    jquery lib

--- index.php
+++ index.php
@@ -88,5 +88,6 @@
 
     </div>
    </body>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
    <script src="bootstrap/js/bootstrap.min.js"></script>
  </html>
