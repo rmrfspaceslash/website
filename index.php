acd129e58b72707a01be593a82b45e332f6c0f24
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 22:49:29 -0400

    remove jumbotron

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
      <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
    </head>
     <body style="background-image: url(https://images.unsplash.com/photo-1460411794035-42aac080490a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80); background-size: cover;  background-repeat: no-repeat;">
-      <div class="jumbotron">
         <div class="container" align="center">
 
        <!--navbar code -->
@@ -43,8 +42,7 @@
       <p class="lead">This is my website for all of my projects and stuff!</p>
       <br><br><br><br><br><br><br><br><br><br><br>
     </div>
-  </div>
-  <div class="container-fluid">
+  <div class="container-fluid bg-light">
     <div class="container">
       <h1>Hello I am Noah Woods</h1>
       <br><br><br><br><br><br><br><br><br><br><br>
