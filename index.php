1fa059f13f915c9ff31c0965fe9d9fdcf9bab39c
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 22:52:59 -0400

    revert previouse changes

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
      <title>Noah's Cloud</title>
      <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
    </head>
-    <body style="background-image: url(https://images.unsplash.com/photo-1460411794035-42aac080490a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80); background-size: cover;  background-repeat: no-repeat;">
+    <body class="">
+      <div class="jumbotron" style="background-image: url(https://images.unsplash.com/photo-1460411794035-42aac080490a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80); background-size: cover;  background-repeat: no-repeat;">
         <div class="container" align="center">
 
        <!--navbar code -->
@@ -42,15 +43,10 @@
       <p class="lead">This is my website for all of my projects and stuff!</p>
       <br><br><br><br><br><br><br><br><br><br><br>
     </div>
-  <div class="container-fluid bg-light">
-    <div class="container">
-      <h1>Hello I am Noah Woods</h1>
-      <br><br><br><br><br><br><br><br><br><br><br>
-      <br><br><br><br><br><br><br><br><br><br><br>
-      <br><br><br><br><br><br><br><br><br><br><br>
-      <br><br><br><br><br><br><br><br><br><br><br>
-      <br><br><br><br><br><br><br><br><br><br><br>
-    </div>
+  </div>
+  <div class="container">
+    <h1>Hello I am Noah Woods</h1>
+    <br><br><br><br><br><br><br><br><br><br><br>
   </div>
 
 
