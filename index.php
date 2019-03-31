164d72d4db151a797a2a0e7de886ab00b83ea28e
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-03-30 23:45:59 -0400

    added href

--- index.php
+++ index.php
@@ -52,13 +52,23 @@
     <h1 class="float-sm-right">Projects</h1><br>
     <hr class="my-4">
     <div class="card-deck">
-      <div class="card" href="#">
-        <div class="card-body" href="#">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-      </div>
       <a href="" style="color: inherit;">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </a>
+      <a href="" style="color: inherit;">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </a>
+    <a href="" style="color: inherit;">
       <div class="card">
         <div class="card-body">
           <h4 class="card-title">Card title</h4>
@@ -66,14 +76,6 @@
         </div>
       </div>
     </a>
-      <div class="card">
-        <div class="card-body">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-        </div>
-      </div>
-    </div>
-  </div>
 
 
 
