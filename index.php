983053f804a1f6ebd9967c316cd0e6435c39d2b8
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-04-01 19:18:56 -0400

    faster fade scroll

--- index.php
+++ index.php
@@ -279,7 +279,7 @@
                fadeElem.each(function() {
                    var $this = $(this),
                        elemOffsetTop = $this.offset().top;
-                   if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/300);
+                   if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/200);
                });
            });
 
