29fd86f5c031b3cb08f99d6224444c20af7618cd
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-04-01 17:35:46 -0400

    contact section update

--- index.php
+++ index.php
@@ -184,16 +184,19 @@
     <div class="container">
       <form>
         <div class="form-group row">
-          <label for="inputEmail3" class="col-sm-2 form-control-label">Email</label>
+          <label for="contactEmail" class="col-sm-2 form-control-label">Email</label>
           <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+            <input type="email" class="form-control" id="contactEmail" placeholder="Email">
           </div>
         </div>
         <div class="form-group row">
-          <label for="inputPassword3" class="col-sm-2 form-control-label">Password</label>
+          <label for="contactMessage" class="col-sm-2 form-control-label">Message</label>
           <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+            <textarea class="form-control" id="contactMessage" rows="4" placeholder="Enter your message here..."></textarea>
           </div>
+        </div>
+        <div class="form-group row">
+          <button class="btn btn-success float-sm-right" type="button" name="contactSubmit" value="Submit Message"></button>
         </div>
       </form>
     </div>
