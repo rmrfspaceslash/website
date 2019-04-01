aa49475cb507a99ff9d97c7013feb24554f6d76f
rmrfspaceslash <noahcwoods@gmail.com>
2019-04-01 11:23:37 -0400

    jumbotron addition

--- index.php
+++ index.php
@@ -79,35 +79,38 @@
     </div><br>
 
     <!-- Begin Project section -->
-    <h1 class="float-sm-right">Projects</h1><br>
-    <hr class="my-4">
+    <div class="jumbotron jumbotron-fluid">
+      <h1 class="float-sm-right">Projects</h1><br>
+      <hr class="my-4">
 
-    <!-- Project card 1 -->
-    <div class="card-deck">
-      <a class="card" href="" style="color: inherit;" data-toggle="modal" data-target=".projmodal1">
+      <!-- Project card 1 -->
+      <div class="card-deck">
+        <a class="card" href="" style="color: inherit;" data-toggle="modal" data-target=".projmodal1">
           <div class="card-body shadow">
             <h4 class="card-title">Noah's Cloud</h4>
             <p class="card-text">This is my website that your currently viewing. Click to find
-            out why I created this site</p>
+              out why I created this site</p>
           </div>
-      </a>
+        </a>
 
-      <!-- Project card 2 -->
-      <a class="card" href="" style="color: inherit;" data-toggle="modal" data-target=".projmodal2">
+        <!-- Project card 2 -->
+        <a class="card" href="" style="color: inherit;" data-toggle="modal" data-target=".projmodal2">
           <div class="card-body shadow">
             <h4 class="card-title">Sunday Night Drive</h4>
             <p class="card-text">This is a podcast focusing on updates in the tech world</p>
           </div>
-      </a>
+        </a>
 
-      <!-- Project card 3 -->
-    <a class="card" href="" style="color: inherit;" data-toggle="modal" data-target=".projmodal3">
-        <div class="card-body shadow">
-          <h4 class="card-title">Web-based Code Editor</h4>
-          <p class="card-text">This editor is an idea I have for a simple web-based IDE</p>
-        </div>
-    </a>
-  </div><br>
+        <!-- Project card 3 -->
+        <a class="card" href="" style="color: inherit;" data-toggle="modal" data-target=".projmodal3">
+          <div class="card-body shadow">
+            <h4 class="card-title">Web-based Code Editor</h4>
+            <p class="card-text">This editor is an idea I have for a simple web-based IDE</p>
+          </div>
+        </a>
+      </div><br>
+    </div>
+
 
 </div>
 
