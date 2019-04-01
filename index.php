6571909c2e8cbbb473d8b36053b8f09dc14a5509
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-04-01 17:38:47 -0400

    button update name

--- index.php
+++ index.php
@@ -196,7 +196,7 @@
           </div>
         </div>
         <div class="float-sm-right w-25">
-          <button class="btn btn-success w-100" type="button" name="contactSubmit" value="Submit Message"></button>
+          <button class="btn btn-success w-100" type="button" name="contactSubmit">Submit Message</button>
         </div>
       </form>
     </div>
