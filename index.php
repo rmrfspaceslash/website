9ddc2cfb8cca86979bbd0ac572ac4d3f6b2911fd
rmrfspaceslash <noahcwoods@gmail.com>
2019-04-02 07:46:15 -0400

    script test

--- index.php
+++ index.php
@@ -289,7 +289,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
 
-<!--
+
    <script>
     $(document).ready(function(){
       $('.card').hover(
@@ -306,5 +306,5 @@
       );
     });
    </script>
--->
+
  </html>
