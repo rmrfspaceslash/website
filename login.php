9b3a945c8aa279007d0aa508fc60391ffa63265a
rmrfspaceslash <noahcwoods@gmail.com>
2019-04-02 19:12:33 -0400

    add login section

--- login.php
+++ login.php
@@ -15,9 +15,19 @@
     <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
   </head>
   <body>
-    <div class="page-scroll">
-
+    <div class="container" align="center">
+      <div class="card">
+        <div class="card-body shadow-lg col-lg-6 col-sm-12">
+          
+        </div>
+      </div>
     </div>
+
+
+
+
+
+    <div class="page-scroll"></div>
   </body>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="bootstrap/js/bootstrap.min.js"></script>
