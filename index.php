5baced4a9831ac1994306439acb3231f2620c824
Noah Woods <39747354+rmrfspaceslash@users.noreply.github.com>
2019-04-01 20:01:22 -0400

    flipping card testing

--- index.php
+++ index.php
@@ -105,6 +105,25 @@
         </div>
       </div><br>
 
+      <div class="card flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="img/holderimg1.jpg" alt="Holder Image">
+          </div>
+          <div class="flip-card-back">
+            <h1>John Doe</h1>
+            <p>Architect & Engineer</p>
+            <p>We love that guy</p>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
       </div>
 
     <!-- Begin Project section -->
