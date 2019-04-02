d2bf561a034b159ac469cee4a85da40120f86842
rmrfspaceslash <noahcwoods@gmail.com>
2019-04-02 11:19:08 -0400

    logo remake

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
         </nav>
 
         <!--Hero text -->
-        <img src="img/holder1.jpg" style="padding-top: 20vh" alt="Noah's Cloud">
+        <img src="img/logo2.png" style="padding-top: 20vh" alt="Noah's Cloud">
       </div>
     </div>
 
