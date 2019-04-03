28075b56d02d764e081927658c100406fc79da5d
rmrfspaceslash <noahcwoods@gmail.com>
2019-04-03 18:13:33 -0400

    recreate log in section

--- login.php
+++ login.php
@@ -19,11 +19,22 @@
       <div class="card shadow-lg col-lg-6 col-sm-12" style="margin-top: 25vh">
         <div class="card-body">
           <div class="card-title text-secondary" align="center">
-            <form class="form-group w-75" action="" method="post">
-              <input class="form-control" type="text" name="logusername" placeholder="Enter your email...">
-              <input class="form-control" type="password" name="logpassword" placeholder="Enter your password...">
-              <input class="form-control" type="submit" name="logsubmit" value="Log In">
-
+            <form>
+              <div class="form-group row">
+                <label for="contactEmail" class="col-sm-2 form-control-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control shadow" placeholder="Email" name="logEmail">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="contactMessage" class="col-sm-2 form-control-label">Password</label>
+                <div class="col-sm-10">
+                  <input class="form-control shadow" type="password" placeholder="Password" name="logPassword"></input>
+                </div>
+              </div>
+              <div class="float-sm-right w-25">
+                <button class="btn btn-success w-100" type="button" name="logSubmit">Log In</button>
+              </div>
             </form>
           </div>
         </div>
