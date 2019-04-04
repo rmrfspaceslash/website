11a574eb18a2ebe01308948e41370abc2df85496
rmrfspaceslash <noahcwoods@gmail.com>
2019-04-04 14:19:45 -0400

    remove login feature

--- index.php
+++ index.php
@@ -34,9 +34,6 @@
               <li class="nav-item">
                 <a class="nav-link btn btn-link" id="scrollContact">Contact</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link btn btn-link" href="login.php" style="color: white;">Login/Register</a>
-              </li>
             </ul>
           </div>
         </nav>
